7769a38611b27e96425d9a370a69184945a1b82b
Darko Divanisov <darkodivanisov3@gmail.com>
2026-03-11 20:05:46 +0100

    bug hunting

--- src/middlewares/authenticated.js
+++ src/middlewares/authenticated.js
@@ -5,7 +5,6 @@ const { AppError } = require("../utils/appError")
 const isAuthenticated = (req, res, next) => {
       console.log('Cookie received:', req.cookies)
       console.log('Session:', req.session)
-      console.log("Full request: 0", req);
       if(!req.user || !req.user._id){
             throw new AppError("Unauthorized. Please log in!", httpStatus.UNAUTHORIZED);
       }
