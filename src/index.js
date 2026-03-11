7d98a9a34a5f36e5e1e755687a549aa47af9aa9e
Darko Divanisov <darkodivanisov3@gmail.com>
2026-03-11 18:32:02 +0100

    minor mistake

--- src/index.js
+++ src/index.js
@@ -30,8 +30,6 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With']
 }));
 
-app.options('*', cors());
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
